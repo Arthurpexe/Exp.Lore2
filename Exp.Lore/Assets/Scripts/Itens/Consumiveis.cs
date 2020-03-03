20fc849e31c58c2cdd9be869daf63d181ad4f535
Arthur Moura <Arthur.moura@sga.pucminas.br>
2020-03-03 17:08:22 -0300

    Pequenas modificações
    não fui muito produtivo mas consegui arrumar alguns pequenos bugs

--- Exp.Lore/Assets/Scripts/Itens/Consumiveis.cs
+++ Exp.Lore/Assets/Scripts/Itens/Consumiveis.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Novo COnsumivel", menuName = "Inventário/Consumivel")]
-
 public class Consumiveis : Item
 {
-    public int vida;
-    public int dano;
-    public int armadura;
+    [SerializeField]
+    int vida;
+    [SerializeField]
+    int dano;
+    [SerializeField]
+    int armadura;
     GameObject player;
     JogadorStats stats;
 
@@ -17,51 +19,33 @@ public class Consumiveis : Item
         player = GameObject.FindGameObjectWithTag("Player");
         stats = player.GetComponent<JogadorStats>();
 
-        
-        
-            if (stats.vidaAtual + vida <= 100)
-            {
-
-                stats.vidaAtual = stats.vidaAtual + vida;
-
+        if (stats.vidaAtual + vida <= 100)
+        {
+            stats.vidaAtual = stats.vidaAtual + vida;
         }
         else
         {
             stats.vidaAtual = 100;
         }
 
-            if (stats.dano + dano <= 100)
-            {
-                stats.dano = stats.dano + dano;
+        if (stats.dano + dano <= 100)
+        {
+            stats.dano = stats.dano + dano;
         }
         else
         {
             stats.dano = 100;
         }
 
-            if (stats.armadura + armadura <= 100)
-            {
-                stats.armadura = stats.armadura + armadura;
+        if (stats.armadura + armadura <= 100)
+        {
+            stats.armadura = stats.armadura + armadura;
         }
         else
         {
             stats.armadura = 100;
         }
         
-
         RemoverDoInventario();
-    }
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Exp.Lore/Assets/Scripts/Utensilhos/Volume.cs
+++ Exp.Lore/Assets/Scripts/Utensilhos/Volume.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Audio;
 
 public class Volume : MonoBehaviour
 {
-    public AudioMixer mixer;
+     public AudioMixer mixer;
 
     public void SetarVolume(float valorSlider)
     {
