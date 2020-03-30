f4e2487371d4deed4216d1a31f928d63fe72a727
Arthurpexe <Arthur.moura@sga.pucminas.br>
2020-03-30 02:16:21 -0300

    merge do branch do luca
    não adicionei o script do controlador inimigo pq eu acho que vai precisar mudar ele todo depois

---
+++ Exp.Lore/Assets/Scripts/Fisica/Teleport.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+	public Transform saida;
+	GameObject player;
+
+	private void Start()
+	{
+		player = ControladorPersonagem.instancia.gameObject;
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		player.transform.position = saida.transform.position;
+	}
+}
